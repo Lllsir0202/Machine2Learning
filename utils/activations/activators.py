5124d5ce160ca34819d02098b0fd7a7c56fda645
Lllsir0202 <q2696127622@outlook.com>
2025-11-12 16:56:24 +0800

    feat:Optimize a better organization

--- utils/activations/activators.py
+++ utils/activations/activators.py
@@ -2,34 +2,26 @@ import numpy as np
 from activation_functions import *
 
 class Activators:
+    ACTIVATION_MAP = {
+        'sigmoid': Sigmoid,
+        'tanh': Tanh,
+        'relu': Relu,
+        'leaky_relu': Leaky_Relu,
+        'softmax': Softmax
+    }
+
     def __init__(self, activation_type: str):
-        self.activation_type = activation_type
+        self.activation_type = activation_type.lower()
+        if self.activation_type not in self.ACTIVATION_MAP:
+            raise ValueError(f"Unsupported activation function: {activation_type}")
+        self.activation_class = self.ACTIVATION_MAP[self.activation_type]
 
     # TODO: Add more activation functions as needed
     def activate(self, x: np.ndarray) -> np.ndarray:
-        if self.activation_type.lower() == 'sigmoid':
-            return Sigmoid.forward(x)
-        elif self.activation_type.lower() == 'tanh':
-            return Tanh.forward(x)
-        elif self.activation_type.lower() == 'relu':
-            return Relu.forward(x)
-        elif self.activation_type.lower() == 'leaky_relu':
-            return Leaky_Relu.forward(x)
-        elif self.activation_type.lower() == 'softmax':
-            return Softmax.forward(x)
-        else:
-            raise ValueError(f"Unknown activation function: {self.activation_type}")
+        return self.activation_class.forward(x)
         
     def deactivate(self, x: np.ndarray) -> np.ndarray:
-        if self.activation_type.lower() == 'sigmoid':
-            return Sigmoid.backward(x)
-        elif self.activation_type.lower() == 'tanh':
-            return Tanh.backward(x)
-        elif self.activation_type.lower() == 'relu':
-            return Relu.backward(x)
-        elif self.activation_type.lower() == 'leaky_relu':
-            return Leaky_Relu.backward(x)
-        elif self.activation_type.lower() == 'softmax':
-            raise ValueError("Softmax does not have a standard backward function.")
-        else:
-            raise ValueError(f"Unknown activation function: {self.activation_type}")
+        if not hasattr(self.activation_class, 'backward'):
+            raise NotImplementedError(f"Backward method not implemented for {self.activation_type}")
+        
+        return self.activation_class.backward(x)
